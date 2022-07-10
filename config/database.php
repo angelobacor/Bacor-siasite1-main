930bc1655900c77a0c4e7eeefcf9315350ba5380
Admiral Bacor <AdmiralBacor@DESTOP-U257B5U>
2022-07-10 22:15:35 +0800

    new

--- config/database.php
+++ config/database.php
@@ -45,39 +45,38 @@ before you begin development.
  */
  'migrations' => 'migrations',
  'connections' => [
-
-    'mysql' => [
-        'driver' => 'mysql',
-        'host' => env('DB_HOST', null),
-        'port' => env('DB_PORT', null),
-        'database' => env('DB_DATABASE', null),
-        'username' => env('DB_USERNAME', null),
-        'password' => env('DB_PASSWORD', null),
-        'unix_socket' => env('DB_SOCKET', ''),
-        'charset' => env('DB_CHARSET',  'utf8'),
-        'collation' => env('DB_COLLATION', 'utf8_unicode_ci'),
-        'prefix' => env('DB_PREFIX', ''),
-        // 'strict' => env('DB_STRICT_MODE', false),
-        // 'engine' => env('DB_ENGINE', null),
-        // 'timezone' => env('DB_TIMEZONE', '+00:00'),],
-        
-        
-    'sqlsrv' => [
-        'driver' => 'sqlsrv',
-        'host' => env('DB_HOST1', null),
-        'port' => env('DB_PORT1', null),
-        'database' =>  env('DB_DATABASE1', null),
-        'username' =>  env('DB_USERNAME1', null),
-        'password' => env('DB_PASSWORD1', null),
-        'charset' => env('DB_CHARSET', 'utf8'),
-        'prefix' => env('DB_PREFIX', ''), ],
-
-        'testing' => [
-        'driver' => 'sqlite',
-        'database' => ':memory:',
-        'host' => database_path('testing.sqlite'),
-        'prefix' => env('DB_PREFIX', ''),
-        ],
-        ],
-    ],
+ 'mysql' => [
+ 'driver' => 'mysql',
+ 'host' => env('DB_HOST', null),
+ 'port' => env('DB_PORT', null),
+ 'database' => env('DB_DATABASE', null),
+ 'username' => env('DB_USERNAME', null),
+ 'password' => env('DB_PASSWORD', null),
+ 'unix_socket' => env('DB_SOCKET', ''),
+ 'charset' => env('DB_CHARSET', 'utf8'),
+ 'collation' => env('DB_COLLATION', 'utf8_unicode_ci'),
+ 'prefix' => env('DB_PREFIX', ''),
+ // 'strict' => 
+env('DB_STRICT_MODE', false),
+ // 'engine' => env('DB_ENGINE', null)
+ // 'timezone' => 
+env('DB_TIMEZONE', '+00:00'),
+ ],
+ 'sqlsrv' => [
+ 'driver' => 'sqlsrv',
+ 'host' => env('DB_HOST1', null),
+ 'port' => env('DB_PORT1', null),
+ 'database' => env('DB_DATABASE1', null),
+ 'username' => env('DB_USERNAME1', null),
+ 'password' => env('DB_PASSWORD1', null),
+ 'charset' => env('DB_CHARSET', 'utf8'),
+ 'prefix' => env('DB_PREFIX', ''),
+ ],
+ 'testing' => [
+ 'driver' => 'sqlite',
+ 'database' => ':memory:',
+ 'host' => database_path('testing.sqlite'),
+ 'prefix' => env('DB_PREFIX', ''),
+ ],
+ ],
 ];
